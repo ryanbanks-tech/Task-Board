a952719d61a26c8e055b2156169db25a3ab4952f
Ryan Banks <bladeddarklord@gmail.com>
2024-05-21 23:30:38 -0500

    altered all code
    - encapsulated logic in get tasks handler
    - getTask is the http handler for /tasks
    - created helper functions to fetch users, projects, and tasks
    - return http 500 for error handling
    - response encoding as json

--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
-// Project represents a Todoist project
 type Project struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
-// Task represents a Todoist task
 type Task struct {
 	ID        string `json:"id"`
 	Content   string `json:"content"`
@@ -30,103 +29,112 @@ type User struct {
 
 var todoistBearerToken string = os.Getenv("TODOIST_BEARER_TOKEN")
 
-func main() {
-
-	// Define a map to store ID-user mappings
+func getTasks(w http.ResponseWriter, r *http.Request) {
 	userMap := make(map[string]string)
 
-	// Construct the HTTP request for projects
+	// Fetch projects
+	projects, err := fetchProjects()
+	if err != nil {
+		http.Error(w, "Error fetching projects", http.StatusInternalServerError)
+		return
+	}
+
+	// Fetch tasks
+	tasks, err := fetchTasks()
+	if err != nil {
+		http.Error(w, "Error fetching tasks", http.StatusInternalServerError)
+		return
+	}
+
+	// Fetch users
+	users, err := fetchUsers()
+	if err != nil {
+		http.Error(w, "Error fetching users", http.StatusInternalServerError)
+		return
+	}
+
+	// Map users
+	for _, user := range users {
+		userMap[user.ID] = user.Name
+	}
+
+	// Create response structure
+	var response struct {
+		Projects []Project `json:"projects"`
+		Tasks    []Task    `json:"tasks"`
+	}
+	response.Projects = projects
+	response.Tasks = tasks
+
+	// Encode response as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func fetchProjects() ([]Project, error) {
 	req, err := http.NewRequest("GET", "https://api.todoist.com/rest/v2/projects", nil)
 	if err != nil {
-		fmt.Println("Error creating project request:", err)
-		return
+		return nil, err
 	}
-
-	// Set the authorization header
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", todoistBearerToken))
 
-	// Construct the HTTP request for project tasks
-	taskReq, err := http.NewRequest("GET", "https://api.todoist.com/rest/v2/tasks", nil)
-	if err != nil {
-		fmt.Println("Error creating project tasks request:", err)
-		return
-	}
-
-	// Set the authorization header for the tasks request
-	taskReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", todoistBearerToken))
-
-	// Construct the HTTP request for project users
-	userReq, err := http.NewRequest("GET", "https://api.todoist.com/rest/v2/projects/2325372207/collaborators", nil)
-	if err != nil {
-		fmt.Println("Error creating project users request:", err)
-		return
-	}
-
-	// Set the authorization header for the usersa request
-	userReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", todoistBearerToken))
-
-	// Make the request
 	client := http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error fetching projects:", err)
-		return
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	// Make the request for project tasks
-	taskClient := http.Client{}
-	taskResp, err := taskClient.Do(taskReq)
-	if err != nil {
-		fmt.Println("Error fetching project tasks:", err)
-		return
-	}
-	defer taskResp.Body.Close()
-
-	// Make the request for project users
-	userClient := http.Client{}
-	userResp, err := userClient.Do(userReq)
-	if err != nil {
-		fmt.Println("Error fetching project users:", err)
-		return
-	}
-	defer userResp.Body.Close()
-
-	// Decode JSON response
 	var projects []Project
-	err = json.NewDecoder(resp.Body).Decode(&projects)
+	if err := json.NewDecoder(resp.Body).Decode(&projects); err != nil {
+		return nil, err
+	}
+	return projects, nil
+}
+
+func fetchTasks() ([]Task, error) {
+	req, err := http.NewRequest("GET", "https://api.todoist.com/rest/v2/tasks", nil)
 	if err != nil {
-		fmt.Println("Error decoding JSON:", err)
-		return
+		return nil, err
 	}
-	var projectTasks []Task
-	err = json.NewDecoder(taskResp.Body).Decode(&projectTasks)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", todoistBearerToken))
+
+	client := http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error decoding project tasks JSON:", err)
-		return
+		return nil, err
 	}
+	defer resp.Body.Close()
 
-	var projectUsers []User
-	err = json.NewDecoder(userResp.Body).Decode(&projectUsers)
+	var tasks []Task
+	if err := json.NewDecoder(resp.Body).Decode(&tasks); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func fetchUsers() ([]User, error) {
+	req, err := http.NewRequest("GET", "https://api.todoist.com/rest/v2/projects/2325372207/collaborators", nil)
 	if err != nil {
-		fmt.Println("Error decoding project users JSON:", err)
-		return
+		return nil, err
 	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", todoistBearerToken))
 
-	// User mapping
-	for _, projectUser := range projectUsers {
-		userMap[projectUser.ID] = projectUser.Name
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
 	}
+	defer resp.Body.Close()
 
-	// Print projects
-	fmt.Println("Projects:")
-	for _, project := range projects {
-		fmt.Printf("ID: %s, Name: %s\n", project.ID, project.Name)
+	var users []User
+	if err := json.NewDecoder(resp.Body).Decode(&users); err != nil {
+		return nil, err
 	}
+	return users, nil
+}
 
-	// Print project tasks
-	fmt.Println("Project Tasks")
-	for _, projectTask := range projectTasks {
-		fmt.Printf("ID: %s, Assignee: %s:%s, Project: %s, Content: %s, Completed: %t\n", projectTask.ID, projectTask.Assignee, userMap[projectTask.Assignee], projectTask.Project, projectTask.Content, projectTask.Completed)
-	}
+func main() {
+	http.HandleFunc("/tasks", getTasks)
+	log.Fatal(http.ListenAndServe(":8990", nil))
 }
